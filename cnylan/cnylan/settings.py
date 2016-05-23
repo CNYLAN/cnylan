c2aae06fc91c49f741e40763d8cbbf181dab6feb
Max Edwards <medwards147@gmail.com>
2016-05-23 07:10:46 -0400

    update to setting

--- cnylan/cnylan/settings.py
+++ cnylan/cnylan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^%etn=%&eq(-q-ep@#mk3+owbbz@8kgaet!ew0w1&!39yts!%$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
